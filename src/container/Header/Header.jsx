5bef205218affc1e0d8fcb45e170f347b16d8801
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2022-03-06 13:24:33 +0200

    nav bar

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
